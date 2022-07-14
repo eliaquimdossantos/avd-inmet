9e7da17b25035df749efd55a85bb0ef18e2d364b
Eliaquim dos Santos <eliaquimdossantos@gmail.com>
2022-07-14 04:32:50 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,61 @@
+
+import './App.css';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import Chart from './Chart'
+import natal from './data/natal.json';
+import apodi from './data/apodi.json';
+import caico from './data/caico.json';
+import calcanhar from './data/calcanhar.json';
+import ipanguacu from './data/ipanguacu.json';
+import macau from './data/macau.json';
+import mossoro from './data/mossoro.json';
+import santa_cruz from './data/santa_cruz.json';
+
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <Tabs>
+        <TabList>
+          <Tab>APODI</Tab>
+          <Tab>CAICÓ</Tab>
+          <Tab>CALCANHAR</Tab>
+          <Tab>IPANGUAÇU</Tab>
+          <Tab>MACAU</Tab>
+          <Tab>MOSSORÓ</Tab>
+          <Tab>NATAL</Tab>
+          <Tab>SANTA CRUZ</Tab>
+        </TabList>
+
+        <TabPanel>          
+          <Chart cityData={apodi} />
+        </TabPanel>
+        <TabPanel>          
+          <Chart cityData={caico} />
+        </TabPanel>
+        <TabPanel>          
+          <Chart cityData={calcanhar} />
+        </TabPanel>
+        <TabPanel>          
+          <Chart cityData={ipanguacu} />
+        </TabPanel>
+        <TabPanel>          
+          <Chart cityData={macau} />
+        </TabPanel>
+        <TabPanel>          
+          <Chart cityData={mossoro} />
+        </TabPanel>
+        <TabPanel>          
+          <Chart cityData={natal} />
+        </TabPanel>
+        <TabPanel>          
+          <Chart cityData={santa_cruz} />
+        </TabPanel>
+      </Tabs>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Chart.js
@@ -0,0 +1,146 @@
+import { Line } from "react-chartjs-2";
+import zoomPlugin from 'chartjs-plugin-zoom';
+
+import {
+  Chart, LineElement, BarElement, PointElement,
+  BarController, BubbleController, DoughnutController,
+  LineController, PieController, PolarAreaController,
+  RadarController, ScatterController, CategoryScale,
+  LinearScale, LogarithmicScale, RadialLinearScale,
+  TimeScale, TimeSeriesScale, Decimation, Filler, Legend,
+  Title, Tooltip, SubTitle
+} from 'chart.js';
+
+Chart.register(
+  LineElement, BarElement, PointElement, BarController,
+  BubbleController, DoughnutController, LineController,
+  PieController, PolarAreaController, RadarController,
+  ScatterController, CategoryScale, LinearScale,
+  LogarithmicScale, RadialLinearScale, TimeScale,
+  TimeSeriesScale, Decimation, Filler, Legend, Title,
+  Tooltip, SubTitle, zoomPlugin
+);
+
+export default function Grafico({ cityData }) {
+  const tempColor = 'rgb(235, 73, 45)'
+  const umidColor = 'rgb(98, 173, 230)'
+
+  const data = {
+    labels: cityData.labels,
+    datasets: [
+      {
+        label: "Média de temperatura c/ chuva",
+        data: cityData.temperatura_c_chuva,
+        fill: false,
+        pointBackgroundColor: tempColor,
+        pointStyle: 'triangle',
+        pointRadius: 4,
+        pointHoverRadius: 7,
+        yAxisID: 'y',
+        showLine: false
+      },
+      {
+        label: "Média de temperatura s/ chuva",
+        data: cityData.temperatura_s_chuva,
+        fill: false,
+        borderColor: 'rgb(235, 72, 45, 0.5)',        
+        pointBackgroundColor: tempColor,
+        backgroundColor: 'rgb(255, 255, 255,0)',
+        pointBorderWidth: 0,
+        yAxisID: 'y',
+        showLine: true,
+        pointRadius: 2,
+        pointHoverRadius: 7,
+        spanGaps: true,
+        tension: 0.2
+      },
+      {
+        label: "Umidade c/ chuva",
+        data: cityData.umidade_c_chuva,
+        fill: false,                
+        pointBackgroundColor: umidColor,        
+        pointStyle: 'triangle',
+        pointRadius: 4,
+        pointHoverRadius: 7,
+        yAxisID: 'y1',
+        showLine: false
+      },
+      {
+        label: "Umidade s/ chuva",
+        data: cityData.umidade_s_chuva,
+        fill: false,
+        borderColor: 'rgb(98, 173, 230, 0.6)',
+        backgroundColor: 'rgb(255, 255, 255,0)',
+        pointBackgroundColor: umidColor,
+        pointBorderWidth: 0,
+        yAxisID: 'y1',
+        showLine: true,
+        pointRadius: 2,
+        pointHoverRadius: 7,
+        spanGaps: true,
+        tension: 0.2
+      }
+    ],
+  }
+
+  const options = {
+    animation: false,
+    responsive: true,
+    aspectRatio: 2.5,
+    maintainAspectRatio: true,
+    scales: {
+      y: {
+        type: 'linear',
+        display: true,
+        position: 'left',
+        title: {
+          text: 'Temperatura (°C)',
+          display: true
+        },
+        suggestedMax: 37,
+        suggestedMin: 22,
+      },
+      y1: {
+        type: 'linear',
+        display: true,
+        position: 'right',
+        title: {
+          text: 'Umidade (%)',
+          display: true
+        },
+        suggestedMax: 100,
+        suggestedMin: 10
+      }
+    },
+    plugins: {
+      legend: {
+        labels: {
+          usePointStyle: true,
+        },
+      },
+      zoom: {
+        zoom: {
+          wheel: {
+            enabled: true // SET SCROOL ZOOM TO TRUE
+          },
+          mode: "x",
+          speed: 100
+        },
+        pan: {
+          enabled: true,
+          mode: "x",
+          speed: 100
+        }
+      }
+    }
+  }
+
+  return (
+    <div>
+      <Line
+        data={data}
+        options={options}        
+      />
+    </div>
+  );
+};
